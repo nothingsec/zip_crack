c074ae59c2ae25fbd57a729ffb4c65585f7561bb
nothing <nothing@nothingdeMacBook-Air.local>
2017-01-22 14:51:28 +0800

    this is a threading zip-file-cracker

---
+++ README.md
@@ -0,0 +1,5 @@
+这是一个多线程爆破zip压缩包的脚本
+使用方法：
+```
+python unzip.py -f file.zip -d dict.txt
+```
---
+++ unzip.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+#coding:utf-8
+#author:nothing
+import zipfile
+import optparse
+from threading import Thread
+def extractFile(zFile,password):
+    try:
+        zFile.extractFile(pwd=password)
+        print '[+] Found Password :' + password
+        exit(0)
+    except:
+        pass
+
+
+def main():
+    parser = optparse.OptionParser("usage%prog "+ "-f <zipfile> -d <dict>")
+    parser.add_option('-f', dest='zname',type='string',help='specify zip file')
+    parser.add_option('-f', dest='dname', type='string', help='specify dict file')
+    (options, args) = parser.parse_args()
+    if (options.zname == None) | (options.dname == None):
+        print parser.usage
+        exit(0)
+    else:
+        zname = options.zname
+        dname = options.dname
+    zFile = zipfile.ZipFile(zname)
+    passFile = open(dname)
+    for line in passFile.readlines():
+        password = line.strip('\n')
+        t = Thread(target=extractFile,args=(zFile,password))
